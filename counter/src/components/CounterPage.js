3c3dc4427ea2e7bb6d18d01e4ad63112141a5b2f
Sandra Fernandz <sandraenmunich@gmail.com>
2023-03-28 07:58:03 +0200

    added input and form

--- counter/src/components/CounterPage.js
+++ counter/src/components/CounterPage.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 
 export const CounterPage = ({ initialCount }) => {
   const [count, setCount] = useState(initialCount);
+  const [valueToAdd, setValueToAdd] = useState(0);
   useEffect(() => {
     console.log(count);
   }, [count]);
@@ -11,15 +12,26 @@ export const CounterPage = ({ initialCount }) => {
   const handleDecrement = () => {
     setCount(count - 1);
   };
+  const handleChange = (event) => {
+    const value = parseInt(event.target.value) || 0;
+    setValueToAdd(value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setCount(count + valueToAdd);
+    setValueToAdd(0);
+  };
   return (
     <div>
       <button onClick={handleIncrement}>Increase</button>
       <button onClick={handleDecrement}>Decrease</button>
       <h4>Count is {count}</h4>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Add a lot!</label>
         <br />
-        <input type="number" />
+        <input type="number" value={valueToAdd || ''} onChange={handleChange} />
+        <br />
+        <button>Add it!</button>
       </form>
     </div>
   );
