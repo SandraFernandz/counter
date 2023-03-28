7dbc0288c4b7d59e5503f369e52e4c55e129571f
Sandra Fernandz <sandraenmunich@gmail.com>
2023-03-28 08:47:45 +0200

    created first reducer

--- counter/src/components/CounterPage.js
+++ counter/src/components/CounterPage.js
@@ -1,35 +1,49 @@
-import { useState, useEffect } from 'react';
+import { useReducer, useEffect } from 'react';
 
 export const CounterPage = ({ initialCount }) => {
-  const [count, setCount] = useState(initialCount);
-  const [valueToAdd, setValueToAdd] = useState(0);
+  // const [count, setCount] = useState(initialCount);
+  // const [valueToAdd, setValueToAdd] = useState(0);
+  const reducer = (state, action) => {
+    return {
+      ...state,
+      count: state.count + 1,
+    };
+  };
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    valueToAdd: 0,
+  });
   useEffect(() => {
-    console.log(count);
-  }, [count]);
+    console.log(state.count);
+  }, [state.count]);
   const handleIncrement = () => {
-    setCount(count + 1);
+    // setCount(count + 1);
   };
   const handleDecrement = () => {
-    setCount(count - 1);
+    dispatch();
   };
   const handleChange = (event) => {
     const value = parseInt(event.target.value) || 0;
-    setValueToAdd(value);
+    // setValueToAdd(value);
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-    setCount(count + valueToAdd);
-    setValueToAdd(0);
+    // setCount(count + valueToAdd);
+    // setValueToAdd(0);
   };
   return (
     <div>
       <button onClick={handleIncrement}>Increase</button>
       <button onClick={handleDecrement}>Decrease</button>
-      <h4>Count is {count}</h4>
+      <h4>Count is {state.count}</h4>
       <form onSubmit={handleSubmit}>
         <label>Add a lot!</label>
         <br />
-        <input type="number" value={valueToAdd || ''} onChange={handleChange} />
+        <input
+          type="number"
+          value={state.valueToAdd || ''}
+          onChange={handleChange}
+        />
         <br />
         <button>Add it!</button>
       </form>
